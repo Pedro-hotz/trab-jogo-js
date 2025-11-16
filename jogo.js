352431d7d6be50259a89a631b6ff94c11981526c
Pedro-hotz <168838577+Pedro-hotz@users.noreply.github.com>
2025-11-16 14:58:34 -0300

    Create tela de inicio | Add funcionalidades para mudar de tela e iniciar o jogo

--- jogo.js
+++ jogo.js
@@ -9,97 +9,178 @@ const contexto = canvas.getContext('2d');
 
 // [Plano de Fundo]
 const planoDeFundo = {
-  spriteX: 390,                             // posição x para fazer o recorte do chão que está na file "sprites.png" 
-  spriteY: 0,                               // posição y == 
-  largura: 275,
-  altura: 204,
-  x: 0,
-  y: canvas.height - 204,               
-  desenha() {   
-    contexto.fillStyle = '#70c5ce';
-    contexto.fillRect(0,0, canvas.width, canvas.height)         // preeenche de azul, atrás das imagens, a tela do começo ao final 
+    spriteX: 390,                             // posição x para fazer o recorte do chão que está na file "sprites.png" 
+    spriteY: 0,                               // posição y == 
+    largura: 275,
+    altura: 204,
+    x: 0,
+    y: canvas.height - 204,
+    desenha() {
+        contexto.fillStyle = '#70c5ce';
+        contexto.fillRect(0, 0, canvas.width, canvas.height)         // preeenche de azul, atrás das imagens, a tela do começo ao final 
 
-    contexto.drawImage(
-      sprites,
-      planoDeFundo.spriteX, planoDeFundo.spriteY,
-      planoDeFundo.largura, planoDeFundo.altura,
-      planoDeFundo.x, planoDeFundo.y,
-      planoDeFundo.largura, planoDeFundo.altura,
-    );
+        contexto.drawImage(
+            sprites,
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            planoDeFundo.x, planoDeFundo.y,
+            planoDeFundo.largura, planoDeFundo.altura,
+        );
 
-    contexto.drawImage(
-      sprites,
-      planoDeFundo.spriteX, planoDeFundo.spriteY,
-      planoDeFundo.largura, planoDeFundo.altura,
-      (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,          // preenche o plano de fundo na tela inteira 
-      planoDeFundo.largura, planoDeFundo.altura,
-    );
-  },
+        contexto.drawImage(
+            sprites,
+            planoDeFundo.spriteX, planoDeFundo.spriteY,
+            planoDeFundo.largura, planoDeFundo.altura,
+            (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,          // preenche o plano de fundo na tela inteira 
+            planoDeFundo.largura, planoDeFundo.altura,
+        );
+    },
 };
 
 const chao = {                          // Objeto chão 
-  spriteX: 0,                           // posição x para fazer o recorte do chão que está na file "sprites.png"                                 
-  spriteY: 610,                         // posição y ==
-  largura: 224,
-  altura: 112,
-  x: 0,
-  y: canvas.height - 112,               // Faz o chão ficar em baixo, grudado na borda do fim
-  desenha: function desenha() {         // Função do objeto que faz o aparecer na posição certa
-    contexto.drawImage(
-      sprites,
-      chao.spriteX, chao.spriteY,
-      chao.largura, chao.altura,
-      chao.x, chao.y,
-      chao.largura, chao.altura,
-    );
+    spriteX: 0,                           // posição x para fazer o recorte do chão que está na file "sprites.png"                                 
+    spriteY: 610,                         // posição y ==
+    largura: 224,
+    altura: 112,
+    x: 0,
+    y: canvas.height - 112,               // Faz o chão ficar em baixo, grudado na borda do fim
+    desenha: function desenha() {         // Função do objeto que faz o aparecer na posição certa
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            chao.x, chao.y,
+            chao.largura, chao.altura,
+        );
 
-    contexto.drawImage(
-      sprites,
-      chao.spriteX, chao.spriteY,
-      chao.largura, chao.altura,
-      (chao.x + chao.largura), chao.y,          // mesma lógica que o plano de fundo 
-      chao.largura, chao.altura,
-    );
-  },
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            (chao.x + chao.largura), chao.y,          // mesma lógica que o plano de fundo 
+            chao.largura, chao.altura,
+        );
+    },
 };
 
 
 const flappyBird = {                           // Objeto flappy bird 
-  spriteX: 0,
-  spriteY: 0,
-  largura: 33,
-  altura: 24,
-  x: 10,
-  y: 50,
-  velocidade: 0, 
-  gravidade: 0.25,
+    spriteX: 0,
+    spriteY: 0,
+    largura: 33,
+    altura: 24,
+    x: 10,
+    y: 50,
+    velocidade: 0,
+    gravidade: 0.25,
 
-  atualiza:function atualiza() { 
-    flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade; // a velocidade aumenta a cada loop chamado
-    flappyBird.y = flappyBird.y + 1; // faz o flappy bird cair 
-  },
+    atualiza: function atualiza() {
+        flappyBird.velocidade = flappyBird.velocidade + flappyBird.gravidade; // a velocidade aumenta a cada loop chamado
+        flappyBird.y = flappyBird.y + 1; // faz o flappy bird cair 
+    },
 
-  desenha: function desenha() {                 // função do objeto, para não ficar repetitivo     
-    contexto.drawImage(
-      sprites,
-      flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
-      flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
-      flappyBird.x, flappyBird.y,
-      flappyBird.largura, flappyBird.altura,
-    );
-  }
+    desenha: function desenha() {                 // função do objeto, para não ficar repetitivo     
+        contexto.drawImage(
+            sprites,
+            flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
+            flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
+            flappyBird.x, flappyBird.y,
+            flappyBird.largura, flappyBird.altura,
+        );
+    }
 }
 
-function loop() {                       // função main
+
+const messagemInicio = {                          // Objeto chão 
+    spriteX: 134,                           // posição x para fazer o recorte do chão que está na file "sprites.png"                                 
+    spriteY: 0,                         // posição y ==
+    largura: 174,
+    altura: 152,
+    x: (canvas.width / 2) - 174 / 2,               // Faz o chão ficar em baixo, grudado na borda do fim
+    y: 50,
+    desenha: function desenha() {         // Função do objeto que faz o aparecer na posição certa
+        contexto.drawImage(
+            sprites,
+            messagemInicio.spriteX, messagemInicio.spriteY,
+            messagemInicio.largura, messagemInicio.altura,
+            messagemInicio.x, messagemInicio.y,
+            messagemInicio.largura, messagemInicio.altura,
+        );
+
+        contexto.drawImage(
+            sprites,
+            chao.spriteX, chao.spriteY,
+            chao.largura, chao.altura,
+            (chao.x + chao.largura), chao.y,          // mesma lógica que o plano de fundo 
+            chao.largura, chao.altura,
+        );
+    },
+};
+
+let activeTela = {}
+
+function updateTela(novaTela) {
+    activeTela = novaTela;
+}
+
+
+
+const telas = {
+    inicio: {
+        desenha() {
+            messagemInicio.desenha();
+            planoDeFundo.desenha();
+            chao.desenha();
+            flappyBird.desenha();
+        },
+        atualiza() {
+
+        },
+        click() {
+            updateTela(telas.jogo) 
+        }
+    }
+};
+
+
+telas.jogo = {
+
     // tem que ter essa ordem pois, precisa desenhar o fundo primeiro, depois o chão e para assim o personagem ser desenhado e aparecer na tela
     // Só que antes de gerar os frames, ele atualiza os valores do personagem 
-  flappyBird.atualiza() // faz o flappy bird cair
-  
-  planoDeFundo.desenha();
-  chao.desenha();
-  flappyBird.desenha();
-
-  requestAnimationFrame(loop);          // função ajuda a criar os frames da melhor forma 
+    desenha() {
+        planoDeFundo.desenha();
+        chao.desenha();
+        flappyBird.desenha();
+    },
+    atualiza() {
+        flappyBird.atualiza() // faz o flappy bird cair
+    }
 }
 
+
+
+function loop() {                       // função main
+
+
+
+
+    activeTela.desenha()            // Chama a função do objeto / tela que está ativa 
+    activeTela.atualiza()           // ==
+
+
+
+
+    requestAnimationFrame(loop);          // função ajuda a criar os frames da melhor forma 
+}
+
+
+window.addEventListener('click', function(){    // Toda vez que ouver um click na tela , muda a tela de inicio, para a tela do jogo
+                                                // ou seja, chama a função updateTela() com o valor do objeto tela.jogo
+    if(activeTela.click){
+        activeTela.click();
+    }
+});
+
+
+updateTela(telas.inicio)                        // muda o valor da tela que está ativa
 loop();                                         // Faz o jogo rodar 
